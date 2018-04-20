bcf1fd8fbe37cf851330add7b0c92c25f7590057
Tony Hirst <tony.hirst@gmail.com>
2018-04-21 00:37:04 +0100

    update postgres init

--- build/postgres/config/config_tm351.sql
+++ build/postgres/config/config_tm351.sql
@@ -10,16 +10,11 @@ GRANT ALL PRIVILEGES ON DATABASE tm351 TO tm351;
 -- Create an admin superuser just in case...
 CREATE USER tm351admin WITH PASSWORD 'tm351admin' SUPERUSER;
 
--- Create an admin superuser just in case...
-CREATE USER tm351user WITH PASSWORD 'tm351user' SUPERUSER;
-
 -- We need to set the oustudent user to a superuser so pg_dump works
 CREATE ROLE oustudent WITH SUPERUSER LOGIN;
 
 
 -- Legacy support
-CREATE USER test PASSWORD 'test';
-CREATE DATABASE tm351test;
-GRANT ALL PRIVILEGES ON DATABASE tm351test TO test;
-
-
+-- CREATE USER test PASSWORD 'test';
+-- CREATE DATABASE tm351test;
+-- GRANT ALL PRIVILEGES ON DATABASE tm351test TO test;
