d98a5282ed75955b54c2ad3685c6d503afab51cc
Tony Hirst <tony.hirst@gmail.com>
2018-04-30 20:45:07 +0100

    reorder items inside checks

--- build/mongo/sharded/mongo_cluster.sh
+++ build/mongo/sharded/mongo_cluster.sh
@@ -15,66 +15,73 @@ mkdir -p /vagrant/logs
 
 MONGOD_PORT=27017
 
-cp $THISDIR/etc/mongo-shards-down /etc/mongo-shards-down
-chown mongodb:mongodb /etc/mongo-shards-down
-chmod a+x /etc/mongo-shards-down
-
-cp $THISDIR/etc/mongo-shards-up /etc/mongo-shards-up
-chown mongodb:mongodb /etc/mongo-shards-down
-chmod a+x /etc/mongo-shards-up
 
 
 if [ ! -f /opt/mongo_shard_perms.done ]
 then
-echo " - set up mongo permissions"
-#Add oustudent user to mongodb group
-usermod -a -G mongodb oustudent
-#Grant permissions on certain commands to mongodb group
-echo '''
-%mongodb    ALL=NOPASSWD:   /usr/bin/killall mongod
-%mongodb    ALL=NOPASSWD:   /usr/bin/killall mongos
-%mongodb    ALL=NOPASSWD:   /bin/systemctl restart mongodb
-%mongodb    ALL=NOPASSWD:   /etc/mongo-shards-down
-%mongodb    ALL=NOPASSWD:   /etc/mongo-shards-up
-'''  >> /etc/sudoers
-touch /opt/mongo_shard_perms.done
+    echo " - set up mongo shard commands"
+
+    cp $THISDIR/etc/mongo-shards-down /etc/mongo-shards-down
+    chown mongodb:mongodb /etc/mongo-shards-down
+    chmod a+x /etc/mongo-shards-down
+
+    cp $THISDIR/etc/mongo-shards-up /etc/mongo-shards-up
+    chown mongodb:mongodb /etc/mongo-shards-down
+    chmod a+x /etc/mongo-shards-up
+
+    echo " - set up mongo permissions"
+    #Add oustudent user to mongodb group
+    usermod -a -G mongodb oustudent
+    #Grant permissions on certain commands to mongodb group
+    echo '''
+    %mongodb    ALL=NOPASSWD:   /usr/bin/killall mongod
+    %mongodb    ALL=NOPASSWD:   /usr/bin/killall mongos
+    %mongodb    ALL=NOPASSWD:   /bin/systemctl restart mongodb
+    %mongodb    ALL=NOPASSWD:   /etc/mongo-shards-down
+    %mongodb    ALL=NOPASSWD:   /etc/mongo-shards-up
+    '''  >> /etc/sudoers
+    
+    touch /opt/mongo_shard_perms.done
 fi
 #--------------
 
-echo "killing mongod and mongos"
-killall mongod
-killall mongos
 
-sleep 3
-if [[ -z "${DOCKERBUILD}" ]]; then
-	systemctl restart mongodb
-fi
-
-sleep 5
-
-echo "removing data files"
-#sudo rm -rf /data/config
-#sudo rm -rf /data/shard*
-
-rm -rf /data/config
-rm -rf /data/shard*
-
-#Create shard folders
-mkdir -p /data/shard0/rs0
-chmod -R a+rwx /data/shard0/rs0
-
-mkdir -p /data/shard1/rs0
-chmod -R a+rwx /data/shard1/rs0
-
-mkdir -p /data/shard2/rs0
-chmod -R a+rwx /data/shard2/rs0
-
-#Create configsvr folder
-mkdir -p /data/config/config-a
-chmod -R a+rwx  /data/config/config-a
 
 ##
 if [ ! -f /opt/mongo_shard_restore.done ]; then
+
+    echo "killing mongod and mongos"
+    killall mongod
+    killall mongos
+
+    sleep 3
+    if [[ -z "${DOCKERBUILD}" ]]; then
+        systemctl restart mongodb
+    fi
+
+    sleep 5
+    
+    #echo "removing data files"
+    #sudo rm -rf /data/config
+    #sudo rm -rf /data/shard*
+
+    rm -rf /data/config
+    rm -rf /data/shard*
+
+    #Create shard folders
+    mkdir -p /data/shard0/rs0
+    chmod -R a+rwx /data/shard0/rs0
+
+    mkdir -p /data/shard1/rs0
+    chmod -R a+rwx /data/shard1/rs0
+
+    mkdir -p /data/shard2/rs0
+    chmod -R a+rwx /data/shard2/rs0
+
+    #Create configsvr folder
+    mkdir -p /data/config/config-a
+    chmod -R a+rwx  /data/config/config-a
+    
     #Fire up the servers
     /etc/mongo-shards-up
 
@@ -107,7 +114,7 @@ if [ ! -f /opt/mongo_shard_restore.done ]; then
     echo "Try a restore..."
     #Should we do this with --drop ?
     #https://docs.mongodb.com/manual/reference/program/mongorestore/#cmdoption--drop
-    mongorestore --numInsertionWorkersPerCollection 2 --port=$MONGOD_PORT /tmpdatafiles/dump-0912 && rm -rf /tmpdatafiles
+    mongorestore --numInsertionWorkersPerCollection 4 --port=$MONGOD_PORT /tmpdatafiles/dump-0912 && rm -rf /tmpdatafiles
     echo "...restore done"
 
     
