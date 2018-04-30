df21aee6f2a228b8f3ca81f0eb3c7af0418de59a
Tony Hirst <tony.hirst@gmail.com>
2018-04-30 20:40:30 +0100

    add distro notebooks

--- build/jupyter-base/build_jupyter.sh
+++ build/jupyter-base/build_jupyter.sh
@@ -38,16 +38,6 @@ if [ ! -f /opt/pybase.done ]; then
     touch /opt/pybase.done
 fi
 
-#Bake test notebooks into the VM
-cp -r $THISDIR/notebooks/ /opt/notebooks/
-
-NB_DIR=/vagrant/notebooks
-export NB_DIR
-
-mkdir -p $NB_DIR
-cp -r /opt/notebooks/ $NB_DIR/buildtest/
-chown $NB_USER:$NB_GID $NB_DIR
-
 
 #Define a custom config dir
 JUPYTERCONFIGDIR=$(/usr/local/bin/jupyter --config-dir)
