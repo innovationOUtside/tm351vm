e8f808b80deba28dd595f591b30745c8a4302846
Tony Hirst <tony.hirst@gmail.com>
2018-07-06 13:21:05 +0100

    Jupyterlab support

---
+++ build/jupyter-custom/jupyter_lab.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -e
+
+echo "Installing Jupyterlab..."
+
+if [ ! -f /opt/jupyter_lab.done ]; then
+
+    apt-get install -y pandoc && apt-get clean
+    
+    for PYTHONVER in 3 ; do
+
+      PYTHON="python$PYTHONVER"
+      PIP="pip$PYTHONVER"
+      #https://stackoverflow.com/questions/49836676/python-pip3-cannot-import-name-main
+      PIP="python3 -m pip"
+
+      $PIP install jupyterlab
+      jupyter serverextension enable --py jupyterlab --sys-prefix
+      
+      #TO DO - service file; start optionally from Vagrantfile?
+
+    done
+    touch /opt/jupyter_lab.done
+fi
+
+echo "...done Jupyterlab"
+
+#restart the service
+#sudo systemctl restart jupyterlab.service
