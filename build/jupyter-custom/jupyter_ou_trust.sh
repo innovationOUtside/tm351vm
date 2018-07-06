e1c25baff8348a763703fd12cfde7c851fa56916
Tony Hirst <tony.hirst@gmail.com>
2018-07-06 13:19:43 +0100

    Simplify trust creation on notebooks

--- build/jupyter-custom/jupyter_ou_trust.sh
+++ build/jupyter-custom/jupyter_ou_trust.sh
@@ -6,14 +6,9 @@ NBDIRECTORY=/vagrant/notebooks
 if  [ -d "$NBDIRECTORY" ]; then
 
     # Trust notebooks in notebook directory
-    files=(`find $NBDIRECTORY -maxdepth 1 -name "*.ipynb"`)
+    files=(`find $NBDIRECTORY/* -maxdepth 2 -name "*.ipynb"`)
     if [ ${#files[@]} -gt 0 ]; then
         jupyter trust $NBDIRECTORY/*.ipynb;
-    fi
-    
-    # Trust notebooks in immediate child directories of notebook directory
-    files=(`find $NBDIRECTORY/* -maxdepth 1 -name "*.ipynb"`)
-    if [ ${#files[@]} -gt 0 ]; then
         jupyter trust $NBDIRECTORY/*/*.ipynb;
     fi
 
