ddc5569991fe7857f957ff99f1fbf0b7836ea019
Tony Hirst <tony.hirst@gmail.com>
2019-09-16 10:33:12 +0100

    Update pip

--- build/base/basepy.sh
+++ build/base/basepy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 #Install base python packages
-apt-get install -y git python3 python3-dev python3-pip &&
+apt-get install -y git python3 python3-dev python3-pip && \
 pip3 install --upgrade pip
